aff9312155cfc81069624e682a1d99723dc94c93
ChobotarCostyantin <chobotar.kostiantyn@chnu.edu.ua>
2025-04-05 23:30:22 +0300

    feat(lab3): Add ArabicToRomanConverter and 31 tests for it

---
+++ Lab3/src/main/java/edu/chobotar/ArabicToRomanConverter.java
@@ -0,0 +1,35 @@
+package edu.chobotar;
+
+import java.util.List;
+
+/*
+  @author User
+  @project Lab3
+  @class ArabicToRomanConverter
+  @version 1.0.0
+  @since 05.04.2025 - 23.10
+*/
+public class ArabicToRomanConverter {
+    public static String arabicToRoman(int number) {
+        if ((number <= 0) || (number > 4000)) {
+            throw new IllegalArgumentException(number + " is not in range (0,4000]");
+        }
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomanNumeral currentSymbol = romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ Lab3/src/main/java/edu/chobotar/Main.java
@@ -0,0 +1,14 @@
+package edu.chobotar;
+
+/*
+  @author Harsteel
+  @project Default (Template) Project
+  @class Main
+  @version 1.0.0
+  @since 05.04.2025 - 23.07
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+    }
+}
